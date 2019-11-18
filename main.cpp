07aed1daed5b01f3a043a74ef42559bec4d2aecf
iTuMaN4iK <swaga.ituman4ik@gmail.com>
2019-11-18 22:45:38 +0500

    test build

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file main.cpp
+ * @author iTuMaN4iK (swaga.ituman4ik@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2019-11-18
+ * 
+ * @copyright Copyright (c) 2019
+ * 
+ */
+#include <iostream>
+
+int main() {
+  std::cout << "Init commit" << std::endl;
+  return 0;
+}
